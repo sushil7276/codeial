c4546d9a815ad38df69310b2a76d463436f20183
Sushil <shil7276@gmail.com>
2023-04-24 11:27:55 +0530

    Nasted population

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,14 @@ const Post = require('../models/post');
 module.exports.home = async (req, res) => {
 
     // Populate the user of each post
-    const post = await Post.find({}).populate('user').exec()
+    const post = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        }).exec()
     return res.render('home', {
         title: "Codeial | Home",
         post

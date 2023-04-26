f204c2e57e17d8d0ed580a532aafe3aade7b5d6b
Sushil <shil7276@gmail.com>
2023-04-26 13:03:53 +0530

    create passport-JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,26 @@
+const passport = require('passport');
+const jWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User = require('../models/user');
+
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey: 'codeial',
+}
+
+passport.use(new jWTStrategy(opts, async function (jwtPayLoad, done) {
+
+    const user = await User.findById(jwtPayLoad._id)
+        .catch(err => console.log("Error in finding user from JWT"))
+
+    if (user) {
+        return done(null, user);
+    }
+    else {
+        return done(null, false);
+    }
+}));
+
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const db = require('./config/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 const { default: mongoose } = require('mongoose');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('connect-flash');
